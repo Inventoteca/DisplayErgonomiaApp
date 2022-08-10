e06da4ff52bc05f5040b089f1c4cce5488d3de8a
Glioce <17276408+Glioce@users.noreply.github.com>
2022-08-10 14:02:07 -0500

    proyecto de react-native con okta, no compila

---
+++ DisplayErgonomiaApp/auth.config.js
@@ -0,0 +1,10 @@
+export default {
+  oidc: {
+    clientId: '0oa51bdiaaDIksD705d7',
+    redirectUri: 'com.okta.dev-81151271:/callback',
+    endSessionRedirectUri: 'com.okta.dev-81151271:/logout',
+    discoveryUri: '	https://dev-81151271.okta.com/oauth2/default',
+    scopes: ['openid', 'profile', 'offline_access'],
+    requireHardwareBackedKeyStore: false,
+  },
+};
